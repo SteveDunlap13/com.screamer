4cbd32a00203c7d2eba53914f9ae571967099e37
SteveDunlap13 <SteveDunlap13@gmail.com>
2015-11-03 19:22:30 -0500

    Testing Aptana Studio GIT integration

--- settings.php
+++ settings.php
@@ -1,38 +1,34 @@
-﻿
-<?php
+﻿<?php
 
-	error_reporting(E_ERROR);
-	ini_set('display_errors', true);
+error_reporting(E_ERROR);
+ini_set('display_errors', true);
 
-  $configFile = "scream-settings.json";
+$configFile = "scream-settings.json";
 
+if (isset($_POST['action'])) {
 
-	if (isset($_POST['action'])) {
+	switch ($_POST['action']) {
 
-		switch ($_POST['action']) {
-    
-			case 'load':
-        load();
-        break;
-        
-			case 'save':
-				save($_POST['settings']);
-				break;
-		}
+		case 'load' :
+			load();
+			break;
+
+		case 'save' :
+			save($_POST['settings']);
+			break;
 	}
+}
 
+function load() {
 
-  function load() {
-  
-    global $configFile;
-    
-    echo file_get_contents($configFile);
-  }
+	global $configFile;
 
-	function save($settings) {
+	echo file_get_contents($configFile);
+}
 
-    global $configFile;
-    echo file_put_contents($configFile, json_encode($settings)) or die("Error opening output file");
-	}
-	
+function save($settings) {
+
+	global $configFile;
+	echo file_put_contents($configFile, json_encode($settings)) or die("Error opening output file");
+}
 ?>
